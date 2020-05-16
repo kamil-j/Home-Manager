c158152ac813db2a27b71d72e26c29638a1a54f9
Kamil Jureczka <kamil.jureczka@hotmail.com>
2020-05-16 18:51:19 +0200

    Better description for the sample sketch

--- examples/homeManager/homeManager.ino
+++ examples/homeManager/homeManager.ino
@@ -1,19 +1,28 @@
+// General settings for MySensors lib
 #define SKETCH_NAME "HomeManager"
 #define SKETCH_VERSION "1.0"
 #define MY_NODE_ID 1
 #define MY_GATEWAY_SERIAL
 
+// Add include to HomeManager library:
 #include <MySensors_HomeManager.h>
+
+// Add includes to entities that you want to use:
 #include <entities/Light.h>
 #include <entities/DHTSensor.h>
 #include <entities/Button.h>
 #include <entities/PIRSensor.h>
 
+// Add a light entity (relay) with homeAssistantId=1 and attached to pin 5.
 Light light(1, 5);
+// Add a button entity (wall switch) attached to pin A2. The button will control light entity.
 Button button(A2, &light);
+// Add a PIR sensor entity with homeAssistantId=2 and attached to pin 4. The PIR sensor will control light entity.
 PIRSensor pirSensor(2, 4, &light);
-
+// Add a DHT sensor entity with homeAssistantId=3 and attached to pin 2.
 DHTSensor dhtSensor(3, 2);
+
+// Main Sketch - below code should be always the same (you don't have to touch it)
 
 void before() {
     homeManager.before();
